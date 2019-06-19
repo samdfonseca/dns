86d9844962337b5ff3bee343e3bb1410fea0507a
Sam Fonseca <sam.fonseca@nytimes.com>
2019-06-19 19:58:44 -0400

    replaces *.composer.samdfonseca.xyz aliases with redirects

--- samdfonseca.xyz.lua
+++ samdfonseca.xyz.lua
@@ -2,6 +2,6 @@ alias(_a, "happy-goldwasser-764097.netlify.com")
 cname(concat("www", _a), "samdfonseca.xyz")
 
 -- composer.samdfonseca.xyz
-alias(concat("prd.composer", _a), "kce64b8531e64fa2e-tp.appspot.com", 300)
-alias(concat("dev.composer", _a), "p2dc37aab76344f7b-tp.appspot.com", 300)
-alias(concat("sam.composer", _a), "c6f5bdd2de7a2d66a-tp.appspot.com", 300)
+redirect(concat("prd.composer", _a), "https://kce64b8531e64fa2e-tp.appspot.com/", 0, 300)
+redirect(concat("dev.composer", _a), "https://p2dc37aab76344f7b-tp.appspot.com/", 0, 300)
+redirect(concat("sam.composer", _a), "https://c6f5bdd2de7a2d66a-tp.appspot.com/", 0, 300)
