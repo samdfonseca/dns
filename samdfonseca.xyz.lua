2212b31893fc2c6c61d5a039956b6e350d273c46
Sam Fonseca <sam.fonseca@nytimes.com>
2019-06-19 19:48:16 -0400

    fixes duplicate dev.composer alias

--- samdfonseca.xyz.lua
+++ samdfonseca.xyz.lua
@@ -4,4 +4,4 @@ cname(concat("www", _a), "samdfonseca.xyz")
 -- composer.samdfonseca.xyz
 alias(concat("prd.composer", _a), "kce64b8531e64fa2e-tp.appspot.com", 300)
 alias(concat("dev.composer", _a), "p2dc37aab76344f7b-tp.appspot.com", 300)
-alias(concat("dev.composer", _a), "c6f5bdd2de7a2d66a-tp.appspot.com", 300)
+alias(concat("sam.composer", _a), "c6f5bdd2de7a2d66a-tp.appspot.com", 300)
