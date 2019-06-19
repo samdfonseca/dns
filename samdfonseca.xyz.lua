eccd02aec3d9693b27926d0c889aa9b65d685a9e
Sam Fonseca <sam.fonseca@nytimes.com>
2019-06-19 19:47:27 -0400

    (dev|prd|sam).composer.samdfonseca.xyz aliases

--- samdfonseca.xyz.lua
+++ samdfonseca.xyz.lua
@@ -1,2 +1,7 @@
 alias(_a, "happy-goldwasser-764097.netlify.com")
 cname(concat("www", _a), "samdfonseca.xyz")
+
+-- composer.samdfonseca.xyz
+alias(concat("prd.composer", _a), "kce64b8531e64fa2e-tp.appspot.com", 300)
+alias(concat("dev.composer", _a), "p2dc37aab76344f7b-tp.appspot.com", 300)
+alias(concat("dev.composer", _a), "c6f5bdd2de7a2d66a-tp.appspot.com", 300)
