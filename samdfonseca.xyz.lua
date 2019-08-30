cb18ce0df363892961322d0b76520c19baea21ae
Sam Fonseca <sam.fonseca@nytimes.com>
2019-08-30 05:31:59 -0400

    switches sam.composer.samdfonseca.xyz to aiqdata-dev-sam-v3 instance

--- samdfonseca.xyz.lua
+++ samdfonseca.xyz.lua
@@ -4,4 +4,4 @@ cname(concat("www", _a), "samdfonseca.xyz")
 -- composer.samdfonseca.xyz
 redirect(concat("prd.composer", _a), "https://kce64b8531e64fa2e-tp.appspot.com/", 0, 300)
 redirect(concat("dev.composer", _a), "https://p2dc37aab76344f7b-tp.appspot.com/", 0, 300)
-redirect(concat("sam.composer", _a), "https://c6f5bdd2de7a2d66a-tp.appspot.com/", 0, 300)
+redirect(concat("sam.composer", _a), "https://m3c170a6d753e5eb0-tp.appspot.com/", 0, 300)
