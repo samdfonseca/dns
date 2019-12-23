583ce10aec2117e9d4db6c6b429cf836a1dee63c
Sam Fonseca <sam.fonseca@nytimes.com>
2019-12-23 11:17:37 -0500

    *.airflow.samdfonseca.xyz

--- samdfonseca.xyz.lua
+++ samdfonseca.xyz.lua
@@ -8,10 +8,6 @@ mailgun(_a, "smtp._domainkey", "k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgK
 redirect(concat("prd.composer", _a), "https://kce64b8531e64fa2e-tp.appspot.com/", 0, 300)
 redirect(concat("dev.composer", _a), "https://p2dc37aab76344f7b-tp.appspot.com/", 0, 300)
 redirect(concat("sam.composer", _a), "https://a1a964803d6f123a1-tp.appspot.com/", 0, 300)
-
--- lt.samdfonseca.xyz
-a("lt", "35.226.96.125")
--- localtunnel server subdomains
-a("*.lt", "35.226.96.125")
-txt(concat("_acme-challenge.lt", _a), "JJ6BIdNiv3O9jgYj6jnOs0jsXiOw8stljXfbvDGEEFw")
-txt(concat("_acme-challenge.lt", _a), "TYC0rOJcuTssnOriZQ9Pn67m3X86gkwas1_KfTKHaL8")
+a("prd-v2.airflow", "35.193.165.39")
+a("sam-v4.airflow", "35.193.165.39")
+txt(concat("_acme-challenge.airflow", _a), "IZJnqwUGEvgRQDLz9ShAJIzSkrsSbw_3TOGmTn1o7Ns")
